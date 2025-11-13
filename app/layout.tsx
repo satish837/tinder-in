0b97ab9e97696df76ee1ffe3dc19cf5b66c7bf75
Satish Kumar <kumar.satish837@gmail.com>
2025-11-13 12:25:47 +0530

    Update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body className="min-h-screen flex flex-col items-center">
         <Script id="structured-data" type="application/ld+json">
